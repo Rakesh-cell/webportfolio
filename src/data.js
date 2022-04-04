b9952cd5093f8764025f2a276a522ad86ee110a1
Rakesh-cell <rakeshedu414@gmail.com>
2022-04-04 11:59:32 +0530

    final commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const projects = [
+    {
+      title: "Employee Feedback app",
+      subtitle: "React Native and Javascript",
+      description:
+        "An simple application for employee feedback",
+      image: require("./ecommerce.png"),
+      link: "https://github.com/Rakesh-cell/neoscrum_app",
+    },
+    {
+      title: "Ecommerce application",
+      subtitle: "React Native and Javascript",
+      description:
+        "An application for Online shopping ",
+      image: require("./social_login.png"),
+      link: "https://github.com/Rakesh-cell/Neostore_app",
+    },
+   
+  ];
+
+  export const skills = [
+      "Javascript","React","python","SQL"
+  ]
+
+  export const testimonials =[
+      {
+          quote: "Good work and Continue your work",
+          name: "Jyoti jabare",
+          image: require("./project-4.gif"),
+          company: "Neosoft Technologies",
+      },
+      
+  ]
